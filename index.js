f1851e82cde949d2801822576940a9d7d359670f
Valeri Karpov <valeri.karpov@10gen.com>
2014-10-22 14:46:27 -0400

    Don't use commander for now

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ var mongodb = require('mongodb');
 var bluebird = require('bluebird');
 var commander = require('commander');
 
-commander.
-  option('-u, --uri', 'MongoDB URI').
-  parse(process.argv);
+var uri = 'mongodb://localhost:27017';
 
-mongodb.MongoClient.connect(commander.uri, function(error, db) {
+mongodb.MongoClient.connect(uri, function(error, db) {
   repl.start({
     prompt: 'nodeshell> '
   });
