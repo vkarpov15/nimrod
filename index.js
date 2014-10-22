ac56714d688dcfc7fc02fa3588d52100ea2db7d5
Valeri Karpov <valeri.karpov@10gen.com>
2014-10-22 15:09:07 -0400

    Testing using yield

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 var repl = require('repl');
 var mongodb = require('mongodb');
-var bluebird = require('bluebird');
+var Promise = require('mpromise');
 var commander = require('commander');
+var co = require('co');
 
-var uri = 'mongodb://localhost:27017';
+var uri = 'mongodb://localhost:27017/test';
 
-mongodb.MongoClient.connect(uri, function(error, db) {
-  repl.start({
+mongodb.MongoClient.connect(uri, function(error, connection) {
+  var db = {};
+
+  Object.defineProperty(db, 'test', {
+    get: function() {
+      var wrapper = {
+        find: function(q) {
+          var p = new Promise;
+          //console.log('running query ' + JSON.stringify(q));
+          connection.collection('test').find(q).toArray(function(error, result) {
+            if (error) {
+              return p.reject(error);
+            }
+            console.log('Got result ' + JSON.stringify(result));
+            p.resolve(result);
+          });
+
+          var result;
+          co(function*() {
+            result = yield p;
+          })();
+          //console.log('Got ' + JSON.stringify(result));
+          return result;
+        }
+      };
+
+      return wrapper;
+    }
+  });
+
+  var replServer = repl.start({
     prompt: 'nodeshell> '
   });
+
+  replServer.context.db = db;
 });
