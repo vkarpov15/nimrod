6764edef216cb52e2dd2cf4bfb4b26c152106b8a
Valeri Karpov <valeri.karpov@10gen.com>
2014-10-24 13:13:03 -0400

    Add scripting mode

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ var uri = 'mongodb://localhost:27017/test';
 var currentFlow;
 var lastCursor;
 
+commander.
+  option('-u, --uri [uri]', 'Database URI [mongodb://localhost:27017]',
+    'mongodb://localhost:27017').
+  option('-f, --file [file]', 'File to run (optional)').
+  parse(process.argv);
+
 function ShellIterator(cursor) {
   var _this = this;
   this.next = function() {
@@ -24,7 +30,7 @@ function ShellIterator(cursor) {
   });
 }
 
-mongodb.MongoClient.connect(uri, function(error, connection) {
+mongodb.MongoClient.connect(commander.uri, function(error, connection) {
   var db = Proxy.create({
     get: function(proxy, collectionName) {
       var wrapper = {
@@ -39,6 +45,10 @@ mongodb.MongoClient.connect(uri, function(error, connection) {
         findOne: function(q) {
           connection.collection(collectionName).findOne(q, currentFlow.add());
           return currentFlow.wait();
+        },
+        count: function(q) {
+          connection.collection(collectionName).count(q, currentFlow.add());
+          return currentFlow.wait();
         }
       };
 
@@ -46,40 +56,53 @@ mongodb.MongoClient.connect(uri, function(error, connection) {
     }
   });
 
-  var replServer = repl.start({
-    prompt: 'nodeshell> ',
-    eval: function(cmd, context, filename, callback) {
-      asyncblock(function(flow) {
-        context.flow = flow;
-        currentFlow = flow;
-        var result = vm.runInContext(cmd, context);
-        if (result instanceof ShellIterator) {
-          lastCursor = result;
-          var documents = [];
-          for (var i = 0; i < 10; ++i) {
-            var doc = result.next();
-            if (!doc) {
-              break;
+  if (commander.file) {
+    asyncblock(function(flow) {
+      this.db = db;
+      this.flow = flow;
+      currentFlow = flow;
+      var script = require('fs').readFileSync(commander.file);
+      var result =
+        vm.runInThisContext(script.toString());
+      console.log('Done executing script ' + commander.file + '!');
+      process.exit(0);
+    });
+  } else {
+    var replServer = repl.start({
+      prompt: 'nodeshell> ',
+      eval: function(cmd, context, filename, callback) {
+        asyncblock(function(flow) {
+          context.flow = flow;
+          currentFlow = flow;
+          var result = vm.runInContext(cmd, context);
+          if (result instanceof ShellIterator) {
+            lastCursor = result;
+            var documents = [];
+            for (var i = 0; i < 10; ++i) {
+              var doc = result.next();
+              if (!doc) {
+                break;
+              }
+              documents.push(doc);
             }
-            documents.push(doc);
+            console.log(JSON.stringify(documents, null, '  '));
+            callback(null, 'Type "it" for more');
+          } else {
+            callback(null, result);
           }
-          console.log(JSON.stringify(documents, null, '  '));
-          callback(null, 'Type "it" for more');
-        } else {
-          callback(null, result);
-        }
-      }, function(error) {
-        if (error) {
-          console.log('Error - ' + error);
-        }
-      });
-    }
-  });
+        }, function(error) {
+          if (error) {
+            console.log('Error - ' + error);
+          }
+        });
+      }
+    });
 
-  replServer.context.db = db;
-  Object.defineProperty(replServer.context, 'it', {
-    get: function() {
-      return lastCursor;
-    }
-  });
+    replServer.context.db = db;
+    Object.defineProperty(replServer.context, 'it', {
+      get: function() {
+        return lastCursor;
+      }
+    });
+  }
 });
