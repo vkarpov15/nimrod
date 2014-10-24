e1190723f94f7b3c8828427a068947a2054051fe
Valeri Karpov <valeri.karpov@10gen.com>
2014-10-24 12:30:45 -0400

    Cursors using fibers

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ var commander = require('commander');
 var vm = require('vm');
 var _ = require('underscore');
 var co = require('co');
+var asyncblock = require('asyncblock');
 
 var uri = 'mongodb://localhost:27017/test';
+var currentFlow;
 
 function CustomPromise() {
   var p = new Promise();
@@ -18,19 +20,20 @@ function CustomPromise() {
   });
 }
 
+function ShellIterator(cursor) {
+  this.next = function() {
+    cursor.nextObject(currentFlow.add());
+    return currentFlow.wait();
+  };
+}
+
 mongodb.MongoClient.connect(uri, function(error, connection) {
   var db = Proxy.create({
     get: function(proxy, collectionName) {
       var wrapper = {
         find: function(q) {
-          var p = new CustomPromise();
-          connection.collection(collectionName).find(q).toArray(function(error, result) {
-            if (error) {
-              return p.reject(error);
-            }
-            p.fulfill(result);
-          });
-          return p;
+          connection.collection(collectionName).find(q, currentFlow.add());
+          return new ShellIterator(currentFlow.wait());
         },
         insert: function(doc) {
           var p = new CustomPromise();
@@ -51,20 +54,32 @@ mongodb.MongoClient.connect(uri, function(error, connection) {
   var replServer = repl.start({
     prompt: 'nodeshell> ',
     eval: function(cmd, context, filename, callback) {
-      var result = vm.runInContext(cmd, context);
-      if (result instanceof CustomPromise) {
-        result.on('fulfill', function(data) {
-          callback(null, data);
-        });
-        result.on('reject', function(error) {
-          console.log('error occurred running ' + cmd);
-          callback(error);
-        });
-      } else {
-        process.nextTick(function() {
+      asyncblock(function(flow) {
+        context.flow = flow;
+        currentFlow = flow;
+        var result = vm.runInContext(cmd, context);
+        if (result instanceof CustomPromise) {
+          result.on('fulfill', function(data) {
+            callback(null, data);
+          });
+          result.on('reject', function(error) {
+            console.log('error occurred running ' + cmd);
+            callback(error);
+          });
+        } else if (result instanceof ShellIterator) {
+          var documents = [];
+          for (var i = 0; i < 10; ++i) {
+            documents.push(result.next());
+          }
+          callback(null, documents);
+        } else {
           callback(null, result);
-        });
-      }
+        }
+      }, function(error) {
+        if (error) {
+          console.log('Error - ' + error);
+        }
+      });
     }
   });
 
