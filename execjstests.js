533d1eb1252124ebf0253589f94b691e3e40d7a4
Valeri Karpov <valeri.karpov@10gen.com>
2015-01-18 16:49:32 -0500

    Add script to execute all jstests

---
+++ execjstests.js
@@ -0,0 +1,19 @@
+var childProcess = require('child_process');
+var fs = require('fs');
+
+var jstests = fs.readdirSync('./jstests/');
+
+var success = 0;
+jstests.forEach(function(file) {
+  console.log('\n\n------');
+  console.log('Executing: ' + file);
+
+  try {
+    childProcess.execSync('node --harmony index.js --file ./jstests/' + file);
+    ++success;
+  } catch(e) {
+    console.log('Test failed: ' + e);
+  }
+});
+
+console.log('Passed: ' + success + '/' + jstests.length);
