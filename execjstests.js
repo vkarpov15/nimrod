ce5f7e47c6c50d4fb89a127a8a9e9e063f36192e
Valeri Karpov <valeri.karpov@10gen.com>
2015-02-11 13:17:25 -0500

    Get rid of Kimono and just clone the mongodb repo

--- execjstests.js
+++ execjstests.js
@@ -1,7 +1,7 @@
 var childProcess = require('child_process');
 var fs = require('fs');
 
-var jstests = fs.readdirSync('./jstests/');
+var jstests = fs.readdirSync('./mongo/jstests/core');
 
 var success = 0;
 jstests.forEach(function(file) {
@@ -9,7 +9,7 @@ jstests.forEach(function(file) {
   console.log('Executing: ' + file);
 
   try {
-    childProcess.execSync('node --harmony --harmony_proxies index.js --file ./jstests/' + file);
+    childProcess.execSync('node --harmony --harmony_proxies index.js --file ./mongo/jstests/core/' + file);
     ++success;
   } catch(e) {
     console.log('Test failed: ' + e);
